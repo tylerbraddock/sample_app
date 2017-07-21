21e8b0ded193da8e8799877d53ddeeaa2b963d6f
Tyler Braddock <tyler.braddock@yahoo.com>
2017-07-21 15:55:10 +0000

    In users_signup_test.rb, add integration test for valid signup information. Refactor flash logic in application.html.erb.

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -14,4 +14,17 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select 'div.field_with_errors'
     assert_select "form[action='/signup']"
   end
+
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post signup_path, params: { user: { name: 'Example User',
+                                  email: 'example@example.com',
+                                  password: 'foobar',
+                                  password_confirmation: 'foobar' } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash.empty?
+  end
 end
