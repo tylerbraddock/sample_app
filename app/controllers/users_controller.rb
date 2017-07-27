a8d32001adf7e84ed7a496d8612ec563318e8f09
Tyler Braddock <tyler.braddock@yahoo.com>
2017-07-27 15:28:59 +0000

    In test/fixtures/users.yml, create new fixture for a user, lucy. In integration/users_edit_test.rb, change tests to include requirement that user must be logged in to access edit form. In sessions_helper.rb, add boolean method for current_user? and methods for use in friendly forwarding. In integration/users_edit_test.rb, write test for successful edit with friendly forwarding to the originally desired url. In users_controller_test.rb, write tests for proper edit and update redirect when not logged in and when logged in but attempting to access edit and update capability of a different user. In sessions_controller.rb create action, add method call for incorporating friendly forwarding functionality. In users_controller.rb, call store_location from logged_in_user method to store originally requested url. Use before_action to call logged_in_user and correct_user method and refactor edit and update actions.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
+
   def new
     @user = User.new
   end
@@ -20,12 +23,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
-
     if @user.update_attributes(user_params)
       flash[:sucess] = "Profile updated"
       redirect_to @user
@@ -33,10 +33,28 @@ class UsersController < ApplicationController
       render :edit
     end
   end
+
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
+    # Before Filters
+
+    # Confirms a logged-in user.
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
+
+    # Confirms the correct user.
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
 end
-
-private
-
-  def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
-  end
