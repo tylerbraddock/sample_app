9671a2aa0ee0812b73823a9da134197eaa79966e
Tyler Braddock <tyler.braddock@yahoo.com>
2017-08-08 18:59:53 +0000

    In microposts.yml, create micropost fixtures. In test/models/microposts_test.rb, add tests for validations and order of retrieval from database. In test/models/user_test.rb, add test for dependent destroy. In model user.rb, implement has_many association with dependent destroy. In model micropost.rb, adjust default_scope order and include validations for user_id and content.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -75,4 +75,12 @@ class UserTest < ActiveSupport::TestCase
   test "authenticated? should return false for a user with nil digest" do
     assert_not @user.authenticated?(:remember, '')
   end
+
+  test "destroying users should destroy associated microposts" do
+    @user.save
+    @user.microposts.create!(content: 'Hello')
+    assert_difference "Micropost.count", -1 do
+      @user.destroy
+    end
+  end
 end
