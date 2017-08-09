267fbc5fd7f2a7ebb08436b648b8320b446c93bd
Tyler Braddock <tyler.braddock@yahoo.com>
2017-08-09 17:41:42 +0000

    In model user.rb, define feed method. In static_pages_controller.rb, add @feed_items variable to home action. Create shared/_feed.html.erb to display microposts on feed. In stat-c_pages/home.html.erb, add layout and rendering of microposts. In microposts_controller.rb create action, set @feed_items to empty array if a micropost fails to save.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,9 @@
 class StaticPagesController < ApplicationController
   def home
-    @micropost = current_user.microposts.build if logged_in?
+    if logged_in?
+      @micropost = current_user.microposts.build
+      @feed_items = current_user.feed.paginate(page: params[:page], :per_page => 25)
+    end
   end
 
   def help
